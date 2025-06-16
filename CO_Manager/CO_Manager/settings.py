2d4604825cdd0d1703bb86cf3b520f3a175eaf67
DoubleTT142025 <thanhtrung230801@gmail.com>
2025-06-16 11:27:36 +0700

    update setting for deploy server

--- CO_Manager/CO_Manager/settings.py
+++ CO_Manager/CO_Manager/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'CO_Manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Add the STATICFILES_DIRS setting to include the 'static' directory
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
